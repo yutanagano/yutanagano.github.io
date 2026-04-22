7a01bfa5df314d3f6aa8e7af3d5c8eb28d97c33c
Yuta Nagano <52748151+yutanagano@users.noreply.github.com>
2026-04-22 10:09:11 +0900

    feat: mobile menu aligns right and has close button

--- assets/ts/nav.ts
+++ assets/ts/nav.ts
@@ -1,10 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   const toggleBtn = document.getElementById("menu-toggle-btn");
+  const hamburger = document.getElementById("hamburger");
+  const xMark = document.getElementById("x-mark");
   const mobileNav = document.getElementById("mobile-nav");
 
-  if (toggleBtn !== null && mobileNav !== null) {
+  if (
+    toggleBtn !== null &&
+    hamburger !== null &&
+    xMark !== null &&
+    mobileNav !== null
+  ) {
     toggleBtn.addEventListener("click", () => {
       mobileNav.classList.toggle("active");
+      hamburger.classList.toggle("hidden");
+      xMark.classList.toggle("hidden");
     });
   }
 });
